3fcfa30f3f5f3802c8f74da4490374c6b2442e8e
Jullyene Patta Ramos <jilesss9023Caju@outlook.com>
2024-11-23 19:27:43 -0300

    Footer

--- adidas/public/components/Footer/Footer.jsx
+++ adidas/public/components/Footer/Footer.jsx
@@ -1,23 +1,23 @@
-// import styles from "./sass/styles.css"
+import styles from "./sass/styles.module.css"
 
 export default function Footer() {
     return (
-        <footer>
-            <section>
+        <footer className={styles.footer}>
+           
 
                 {/* Explore */}
-                <div>
-                    <p>Explore</p>
-                    <div>
+                <div className={styles.texts}>
+                    <p  className={styles.titleDiv}>Explore</p>
+                    <div className={styles.sectionSobre}>
                         <a href="#"> Encontre uma loja Adidas</a>
                         <a href="#">Cadastre-se para receber Novidades</a>
                         <a href="#">Adidas Adiclub</a>
                     </div>
                 </div>
 
-                <div>
-                    <p>Ajuda</p>
-                    <div>
+                <div className={styles.texts}>
+                    <p className={styles.titleDiv}>Ajuda</p>
+                    <div className={styles.sectionSobre}>
                         <a href="#"> Dúvidas Gerais</a>
                         <a href="#">Encontre seu tamanho</a>
                         <a href="#">Fale conosco</a>
@@ -25,17 +25,19 @@ export default function Footer() {
                 </div>
 
 
-                <div>
-                    <p>Redes Sociais</p>
-                    <img src="../../assets/png/youtube.png" alt="icone-youtube" />
-                    <img src="../../assets/png/facebook.png" alt="icone-facebook" />
-                    <img src="../../assets/png/instagram.png" alt="icone-instagram" />
-                    <img src="../../assets/png/pinterest.png" alt="icone-pinterest" />
-                    <img src="../../assets/png/tiktok.png" alt="icone-tiktok" />
-                    <img src="../../assets/png/twitter.png" alt="icone-twitter" />
+                <div className={styles.texts}>
+                    <p className={styles.titleDiv}>Redes Sociais</p>
+                    <div className={styles.redesFooter}>
+                        <img src="../../assets/png/youtube.png" alt="icone-youtube" />
+                        <img src="../../assets/png/facebook.png" alt="icone-facebook" />
+                        <img src="../../assets/png/instagram.png" alt="icone-instagram" />
+                        <img src="../../assets/png/pinterest.png" alt="icone-pinterest" />
+                        <img src="../../assets/png/tiktok.png" alt="icone-tiktok" />
+                        <img src="../../assets/png/twitter.png" alt="icone-twitter" />
+                    </div>
                 </div>
 
-            </section>
+          
         </footer>
     )
 }
